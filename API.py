4ede6c6de94f50d68be3def2000c3b70210481f3
Askkyy <ask.ask.air@gmail.com>
2023-03-10 14:05:28 -1000

    first commit

---
+++ API.py
@@ -0,0 +1,117 @@
+import requests, json
+from prettytable import PrettyTable
+
+url = "https://api.notion.com/v1/pages"
+
+token = 'secret_LSj6NUeRXVHJSB1oyIR0uy7zPYlIEIhgoHnBo2bmvtE'
+databaseId = '36c218d22aa34ec0be75ac4946c96135'
+
+headers = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
+    "Notion-Version": "2021-05-13"
+}
+
+def readDatabase(databaseId, headers):
+    readUrl = f"https://api.notion.com/v1/databases/{databaseId}/query"
+
+    res = requests.request("POST", readUrl, headers=headers)
+    data = res.json()
+    print(res.status_code)
+    # print(res.text)
+
+    with open('./db.json', 'w', encoding='utf8') as f:
+        "{}".format(json.dump(data, f, ensure_ascii=False,indent=2))
+    with open('./db.json', 'r') as f:
+      data = json.load(f)
+    #print("{}".format(json.dumps(data,indent=2)))
+    print(data["results"][1]["properties"]["Japanese"]["rich_text"][0]["plain_text"])
+
+    result_len = len(data["results"])
+    print(result_len)
+    selected_rows = []
+    for i in range(result_len):
+      a = []
+      japanese = data["results"][i]["properties"]["Japanese"]["rich_text"][0]["plain_text"]
+      english = data["results"][i]["properties"]["English"]["title"][0]["plain_text"]
+      a.append(japanese)
+      a.append(english)
+      selected_rows.append(a)
+    print(selected_rows)
+
+    table = PrettyTable()
+    table.field_names = ["Japanese", "Englsih"]
+    for row in selected_rows:
+        table.add_row([row[0], row[1]])
+    print(table)
+
+readDatabase(databaseId, headers)
+
+def createPage(databaseId, headers):
+
+    createUrl = 'https://api.notion.com/v1/pages'
+
+    newPageData = {
+        "parent": { "database_id": databaseId },
+        "properties": {
+            "Description": {
+                "title": [
+                    {
+                        "text": {
+                            "content": "Review"
+                        }
+                    }
+                ]
+            },
+            "Value": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Amazing"
+                        }
+                    }
+                ]
+            },
+            "Status": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Active"
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    data = json.dumps(newPageData)
+    # print(str(uploadData))
+
+    res = requests.request("POST", createUrl, headers=headers, data=data)
+
+    print(res.status_code)
+    print(res.text)
+
+def updatePage(pageId, headers):
+    updateUrl = f"https://api.notion.com/v1/pages/{pageId}"
+
+    updateData = {
+        "properties": {
+            "Value": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Pretty Good"
+                        }
+                    }
+                ]
+            }        
+        }
+    }
+
+    data = json.dumps(updateData)
+
+    response = requests.request("PATCH", updateUrl, headers=headers, data=data)
+
+    print(response.status_code)
+    print(response.text)
--- app.py
+++ app.py
@@ -7,17 +7,6 @@ import pytz
 # create the app
 app = Flask(__name__)
 
-# create the extension
-db = SQLAlchemy()
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
-db.init_app(app)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), nullable=False)
-    body = db.Column(db.String(300), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False,default = datetime.now(pytz.timezone('US/Hawaii')) )
 
 @app.route("/",methods=['GET','POST'])
 def index():
@@ -43,22 +32,6 @@ def create():
         else:
             return  render_template('create.html')
 
-@app.route("/<int:id>/update",methods=['GET','POST'])
-def update(id ):
-    with app.app_context():
-        post = Post.query.get(id)
-        if request.method =='GET':
-            return render_template('update.html',post=post)
-        else:
-            post.title = request.form.get('title')
-            post.body = request.form.get('body')
-            db.session.commit()
-            return redirect('/')
 
-@app.route("/<int:id>/delete",methods=['GET'])
-def delete(id):
-    with app.app_context():
-        post = Post.query.get(id)
-        db.session.delete(post)
-        db.session.commit()
-        return redirect('/')
+
+
