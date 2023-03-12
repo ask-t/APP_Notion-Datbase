ec176b5647f31818449e26c49793709afea55c9c
Askkyy <ask.ask.air@gmail.com>
2023-03-11 19:11:57 -1000

    add a function csv

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from flask import Flask
-from flask import render_template,request,redirect
+from flask import render_template,request,redirect,send_file
 import API
 import pandas as pd
 # create the app
@@ -12,13 +12,20 @@ def index():
     with app.app_context():
         if request.method =='GET':
             return  render_template('index.html')
-        if request.method == 'POST':
-            text_en = request.form.get('en')
-            text_jp = request.form.get('jp')
-            df = API.createPage(text_en,text_jp)
-            header = df.columns
-            record = df.values.tolist()
-            return render_template('index.html' , text_en = text_en, text_jp=text_jp,header=header, record=record )
+        elif request.method == 'POST':
+            if request.form["button"]  == "create new":
+                text_en = request.form.get('en')
+                text_jp = request.form.get('jp')
+                df = API.createPage(text_en,text_jp)
+                header = df.columns
+                record = df.values.tolist()
+                return render_template('index.html' , text_en = text_en, text_jp=text_jp,header=header, record=record )
+            elif request.form["button"] == "CSV":
+                API.create_csv()
+                text_en = "Succeed to create CSV"
+                downloadFile = "to_csv_out.csv"
+                
+                return send_file(downloadFile,as_attachment = True)
 
 
 
